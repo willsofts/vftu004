8d44531a3c64f93ac44ce087069d0cfd6f673f67
tassan_oro <32536248+tassun@users.noreply.github.com>
2025-06-01 10:59:02 +0700

    using config from app.config.js

---
+++ app.config.js
@@ -0,0 +1,14 @@
+const appVariables = {
+    API_URL: "http://127.0.0.1:8080",
+    BASE_URL: "http://127.0.0.1:8080",
+    IMG_URL: "http://localhost:8080",
+    MULTI_LANGUAGES: ["EN","TH"],
+    BASE_CSS: "./css/theme_green_sea.css"
+};
+function getAppConfigs() {
+    return appVariables;
+}
+function getAppConfig(key) {
+    return appVariables[key];
+}
+console.log("appVariables",appVariables);
