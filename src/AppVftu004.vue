de4404738014f800b1a0476452a5e67533862c2b
tassan_oro <32536248+tassun@users.noreply.github.com>
2025-06-14 15:15:49 +0700

    default theme metronic

--- src/AppVftu004.vue
+++ src/AppVftu004.vue
@@ -2,7 +2,7 @@
 <template>
   <div id="fswaitlayer" class="fa fa-spinner fa-spin"></div>
   <div class="pt-page pt-page-current pt-page-controller search-pager">
-    <PageHeader ref="pageHeader" :labels="labels" pid="vftu004" version="1.0.0" showLanguage="true" @language-changed="changeLanguage" :multiLanguages="multiLanguages" :build="buildVersion" />
+    <PageHeader ref="pageHeader" :labels="labels" pid="vftu004" version="1.0.0" showLanguage="true" @language-changed="changeLanguage" :multiLanguages="multiLanguages" :build="buildVersion" :visible="displayPageHeader" />
     <SearchForm ref="searchForm" :labels="labels" :dataCategory="dataCategory" @data-select="dataSelected" @data-insert="dataInsert" />
   </div>
   <teleport to="#modaldialog">
@@ -14,7 +14,7 @@ import { ref } from 'vue';
 import { PageHeader } from '@willsofts/will-control';
 import SearchForm from '@/components/SearchForm.vue';
 import EntryForm from '@/components/EntryForm.vue';
-import { getLabelModel, getMultiLanguagesModel } from "@willsofts/will-app";
+import { getLabelModel, getMultiLanguagesModel, getMetaInfo } from "@willsofts/will-app";
 import { getDefaultLanguage, setDefaultLanguage } from "@willsofts/will-app";
 import { startApplication } from "@willsofts/will-app";
 
@@ -26,7 +26,8 @@ export default {
   setup() {
     let labels = ref(getLabelModel());
     const multiLanguages = ref(getMultiLanguagesModel());
-    return { buildVersion, multiLanguages, labels };
+	const displayPageHeader = !(String(getMetaInfo().DISPLAY_PAGE_HEADER)=="false");
+    return { displayPageHeader, buildVersion, multiLanguages, labels };
   },
   mounted() {
     console.log("App: mounted ...");
